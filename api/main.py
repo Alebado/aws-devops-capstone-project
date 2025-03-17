ddd2c684c761b3286493c8312f9a5b3cc5c383fb
Alebado <albertkirakosiani@gmail.com>
2025-03-18 02:16:07 +0400

    added GH Action for docker

--- api/main.py
+++ api/main.py
@@ -29,7 +29,7 @@ s3 = boto3.client(
     aws_access_key_id= os.getenv("AWS_ACCESS_KEY"),
     aws_secret_access_key= os.getenv("AWS_SECRET_KEY"))
 
-bucket_name = 'YOUR_BUCKET_NAME' # Add your bucket name here
+bucket_name = 'capstone-roject' # Add your bucket name here
 
 @app.post("/generate-qr/")
 async def generate_qr(url: str):
@@ -52,10 +52,10 @@ async def generate_qr(url: str):
 
     # Generate file name for S3
     file_name = f"qr_codes/{url.split('//')[-1]}.png"
-
+    print("does it work?")
     try:
         # Upload to S3
-        s3.put_object(Bucket=bucket_name, Key=file_name, Body=img_byte_arr, ContentType='image/png', ACL='public-read')
+        s3.put_object(Bucket=bucket_name, Key=file_name, Body=img_byte_arr, ContentType='image/png')
         
         # Generate the S3 URL
         s3_url = f"https://{bucket_name}.s3.amazonaws.com/{file_name}"
